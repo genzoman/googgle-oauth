792a49d13ce04eb5fb011841bf79becd6fdb09ec
Darrin Cecil <darrin@maketime.io>
2017-04-30 12:36:58 -0400

    simple working click to send to google vision

--- routes/visionRoutes.js
+++ routes/visionRoutes.js
@@ -2,13 +2,19 @@ const express = require("express");
 const visionRouter = express.Router();
 const fs = require("bluebird").promisifyAll(require("fs"));
 var images = fs.readdirAsync("./images");
+const vision = require("../services/cloud-vision");
 visionRouter.get("/", (req, res) => {
   images.then(data => {
     res.render("vision", {
       files: data
     })
   })
-})
-
+});
+const imageUrl = "/Users/darrincecil/projects/passport-google-oauth2-example/images/snek.jpg";
+visionRouter.post("/", (req, res) => {
+  vision.detectText(imageUrl, (err, text) => {
+    res.send(text);
+  })
+});
 module.exports = visionRouter;
 ///Users/darrincecil/projects/passport-google-oauth2-example/images
