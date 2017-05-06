e4b7c5a21f1156e2799d4f1296e4bb3aaed8c989
Darrin Cecil <darrin@maketime.io>
2017-05-06 10:09:18 -0400

    allow file to keep original name

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -2,11 +2,20 @@ var express = require('express')
   , router = express.Router()
 
 var multer = require("multer");
-var upload = multer({ dest: "uploads/"})
+const storage = multer.diskStorage({
+  destination: (req, files, cb) => {
+    cb(null, __dirname);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+var upload = multer({ storage: storage });
 router.get("/", (req, res, next) => {
   res.render("upload");
 })
-router.post('/', upload.any(), function(req, res) {
+router.post('/', upload.any(), function (req, res) {
   res.redirect("/");
 });
 module.exports = router;
