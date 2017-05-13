da30da9d016de26a5a83c2f7cb221eac686794b9
Darrin Cecil <darrin@maketime.io>
2017-05-13 13:34:02 -0400

    transform stream

--- streams.js
+++ streams.js
@@ -3,5 +3,13 @@ const request = require("request");
 const fetch = require("node-fetch");
 const btoa = require("btoa");
 let read = fs.createReadStream("./package.json", "utf-8");
-let write = fs.createWriteStream("./results.txt");
-read.pipe(write)
+let write = fs.createWriteStream("./results1.txt");
+const through2 = require("through2");
+
+read.pipe(through2((chunk, enc, cb) => {
+  let data = chunk.toString();
+  cb(null, data += "123");
+}))
+.pipe(write);
+
+
